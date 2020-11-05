c2df5ba83d8ea2c7fc1075bbc13342f02148d91f
AZC <albertzcheng@gmail.com>
2020-11-04 21:10:25 -0800

    added date and icon

--- js/app.js
+++ js/app.js
@@ -16,32 +16,48 @@ function showCityWeather(e) {
             var cityTemperature = Number.parseFloat(city.main.temp).toFixed(1);
             var longitude = city.coord.lon;
             var latitude = city.coord.lat;
-            console.log(city)
-            $(".cityTitleName").text(city.name);
+            var currentDate = showDate(city.dt)
+            console.log(currentDate);
+            // console.log(date);
+            // console.log(month);
+            // console.log(year);
+            console.log(longitude);
+            console.log(latitude);
+            console.log(city.weather[0].icon)
+            $(".cityTitleName").text(city.name + " (" + currentDate + ")");
             $(".cityTemp").text("Temperature: " + cityTemperature.toString() + " " + String.fromCharCode(176) + "F");
             $(".cityHumidity").text("Humidity: " + city.main.humidity + "%");
             $(".cityWindSpeed").text("Wind Speed: " + city.wind.speed + " MPH");
-            showUVIndex(latitude, longitude)
-            showCityForecast(cityName);
+            $(".titleIcon").attr("src", "http://openweathermap.org/img/wn/" + city.weather[0].icon + "@2x.png")
+            // showUVIndex(latitude, longitude)
+            showCityForecast(latitude, longitude);
         });
 
 }
 
-function showUVIndex(lat, lon) {
-    var requestURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=d3704233aa04b4a5db706d0f4fce1f31"
-
-    fetch(requestURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-
+function showDate(dt) {
+    var myDate = new Date(Date(dt));
+    var date = myDate.getDate();
+    var month = myDate.getMonth() + 1;
+    var year = myDate.getFullYear();
+    return String(month) + "/" + String(date) + "/" + String(year);
 }
 
-function showCityForecast(cityName) {
-    var requestURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=3&units=imperial&appid=d3704233aa04b4a5db706d0f4fce1f31"
+// function showUVIndex(lat, lon) {
+//     var requestURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=d3704233aa04b4a5db706d0f4fce1f31"
+
+//     fetch(requestURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+
+// }
+
+function showCityForecast(lat, lon) {
+    var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=d3704233aa04b4a5db706d0f4fce1f31"
     fetch(requestURL)
         .then(function (response) {
             return response.json();
